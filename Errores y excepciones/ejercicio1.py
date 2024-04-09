32e05bcc7eef485efd3740d97f8a166d40befaea
acarmfer <acarmfer@myuax.com>
2024-04-09 18:56:04 +0200

    n

---
+++ Errores y excepciones/ejercicio1.py
@@ -0,0 +1,4 @@
+try:
+    resultado = 10/0
+except ZeroDivisionError:
+    print("Error: Division by zero is not allowed.")
---
+++ Errores y excepciones/ejercicio2.py
@@ -0,0 +1,6 @@
+lista = [1, 2, 3, 4, 5]
+
+try:
+    lista[10]
+except IndexError:
+    print("Error: Index out of range. Please provide a valid index.")
