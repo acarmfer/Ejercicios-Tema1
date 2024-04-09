b53f8ee1e28e4675935d6cbfcf8f9772ed360b14
acarmfer <acarmfer@myuax.com>
2024-04-09 17:03:04 +0200

    m

--- Introducción Informal/ejercicio3.py
+++ Introducción Informal/ejercicio3.py
@@ -2,14 +2,15 @@ numero_1 = 9
 numero_2 = 3
 numero_3 = 6
 
-media = (numero_1 + numero_2 + numero_3) / 3  # Corrección: la división se realiza después de sumar los números
-print("La nota media es", media)
-
- nota_1 = 10
+nota_1 = 10
 nota_2 = 7
 nota_3 = 4
 
-# Calculando la nota final
+# Calculating the average
+media = (numero_1 + numero_2 + numero_3) / 3  # Correction: the division is performed after summing the numbers
+print("La nota media es", media)
+
+# Calculating the final grade
 nota_final = nota_1 * 0.15 + nota_2 * 0.35 + nota_3 * 0.50
 
 print("La nota final es:", nota_final)
